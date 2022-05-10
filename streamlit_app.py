6e2908463fbd218042935887feacdf3cedeb6b79
Raja N <105250725+rnlearning2022@users.noreply.github.com>
2022-05-09 20:38:29 -0400

    Update streamlit_app.py
    added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.subheader('Breakfast Menu Subheader')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-
+streamlit.text('Kale, Sprinach & Rocket Smoothie)
+streamlit.text('Hard-boiled Free-range Egg')
