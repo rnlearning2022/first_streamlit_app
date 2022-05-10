0213887968deaedcc5658c7f94529017500ea956
Raja N <105250725+rnlearning2022@users.noreply.github.com>
2022-05-09 20:44:49 -0400

    Update streamlit_app.py
    updated missing single quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.header(Breakfast Menu Favorites')
+streamlit.header('Breakfast Menu Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Sprinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-range Egg')
