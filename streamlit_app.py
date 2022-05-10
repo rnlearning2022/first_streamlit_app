1bc0210fc181ebb617a55bf14dba7fc3ce40ff6c
Raja N <105250725+rnlearning2022@users.noreply.github.com>
2022-05-09 20:36:34 -0400

    Update streamlit_app.py
    added subheader and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.subheader('Breakfast Menu Subheader')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
