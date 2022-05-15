1d3200dc57c287265f498fbfe778e4a5e2aa1495
Raja N <105250725+rnlearning2022@users.noreply.github.com>
2022-05-15 09:40:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.dataframe(my_data_rows)
 
 #Second text entry box
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
-streamlit.write('The user entered', fruit_choice)
+streamlit.write('Thanks for adding ', fruit_choice)
